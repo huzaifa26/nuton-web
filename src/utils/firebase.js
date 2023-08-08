3b8999d83cc4f4563448062bec44cdc536c9b1c2
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-08-08 23:53:46 +0500

    fix

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,27 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-export const app = initializeApp({
-  apiKey: "AIzaSyAciZqASRT_0f4gGW5Ct6JhYlDBGdvgrfI",
-  authDomain: "mobile-28752.firebaseapp.com",
-  projectId: "mobile-28752",
-  storageBucket: "mobile-28752.appspot.com",
-  messagingSenderId: "241130855958",
-  appId: "1:241130855958:web:b9627b8f2c409cb5ef1f37"
-});
+
+// const config={
+//   apiKey: "AIzaSyAciZqASRT_0f4gGW5Ct6JhYlDBGdvgrfI",
+//   authDomain: "mobile-28752.firebaseapp.com",
+//   projectId: "mobile-28752",
+//   storageBucket: "mobile-28752.appspot.com",
+//   messagingSenderId: "241130855958",
+//   appId: "1:241130855958:web:b9627b8f2c409cb5ef1f37"
+// }
+
+const config = {
+  apiKey: "AIzaSyBTdd7a8mCa4ccnQZ7cY7FPoVQMBByZ7wI",
+  authDomain: "egeria-fd634.firebaseapp.com",
+  projectId: "egeria-fd634",
+  storageBucket: "egeria-fd634.appspot.com",
+  messagingSenderId: "999099935416",
+  appId: "1:999099935416:web:b6fecef066c51e790ae24d",
+  measurementId: "G-JJLX6ZYRSR"
+};
+
+export const app = initializeApp(config);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
